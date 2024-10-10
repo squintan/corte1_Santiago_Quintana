d1bffea47adcba5bc844f97645470f37b84c8dcc
squintant <squintana@uamv.edu.nissh-keygen -t rsa -b 1111 -C squitana@uamv.edu.ni>
2024-10-09 18:55:39 -0600

    EXAMEN

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,10 @@ use App\Http\Controllers\ProductoController;
 
 Route::get('productos', [ProductoController::class, 'index']); // Obtener todos los productos
 Route::post('productos', [ProductoController::class, 'store']); // Crear un nuevo producto
+Route::get('productos/{nombre}', [ProductoController::class, 'showName']); // Obtener un producto específico
 Route::get('productos/{id}', [ProductoController::class, 'show']); // Obtener un producto específico
 Route::put('productos/{id}', [ProductoController::class, 'update']); // Actualizar un producto específico
 Route::delete('productos/{id}', [ProductoController::class, 'destroy']); // Eliminar un producto específico
-Route::get('productos/{nombre}', [ProductoController::class, 'showName']); // Obtener un producto específico
+
 
 
